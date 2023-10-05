91938340b31c169147f389f91042a509e3756462
MAAzad123 <143494752+MAAzad123@users.noreply.github.com>
2023-10-05 17:05:48 +0600

    Add files via upload

---
+++ array.php
@@ -0,0 +1,19 @@
+<?php
+$posts = [
+    [
+        "id" => 1,
+        "title" => "Demo Title"
+    ],
+    [
+        "id" => 2,
+        "title" => "Demo Title"
+    ],
+    [
+        "id" => 3,
+        "title" => "Demo Title"
+    ]
+];
+
+echo '<pre>';
+print_r($posts);
+echo '</pre>';
